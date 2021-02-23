42a4f2665f4d5ed29c71837e52b28d9f1a5afcdb
IsaacRuchalski <isaac.ruchalski@gmail.com>
2021-02-23 13:14:36 +0100

    add compos
    add compo

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', loadChildren: () => import('./instrument/instrument.module').then(m => m.InstrumentModule)},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
