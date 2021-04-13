b9f154d1ed67f8ec7164e5e979b9e7f54366bccf
IsaacRuchalski <isaac.ruchalski@gmail.com>
2021-04-13 16:46:38 +0200

    prepare for stack overflow

--- app/src/app/core/services/http/popup.service.ts
+++ app/src/app/core/services/http/popup.service.ts
@@ -1,54 +1,27 @@
-import { Injectable } from '@angular/core';
-import { Instrument } from '../../models/instrument';
-import { InstrumentService } from './instrument.service';
+import {Injectable} from "@angular/core";
+import {FeatureGroup} from "leaflet";
+import {Observable} from "rxjs";
+import {Instrument} from "../../models/instrument";
+import {InstrumentService} from "./instrument.service";
 
-@Injectable({
-  providedIn: 'root'
-})
-
-
+@Injectable({providedIn: "root"})
 export class PopupService {
+  public instruments?: Instrument[];
+  public html: string = "";
+  constructor(private instrumentService : InstrumentService) {}
 
+  makePopup(name : string): string {
+    this.instruments = [];
+    var html = "";
+    this.instrumentService.getByOrigin(name).subscribe((instruments) => {
+      this.instruments = instruments;
+      console.log(instruments);
 
-
-  private instruments?: Instrument[];
-
-  constructor(private instrumentService: InstrumentService) { 
-
-    
-
-
-  }
-
-
-  makePopup(name:any): string {
-
-   var country = name.properties.ADMIN
-    
-
-   
-    return(this.formatPopup(country))
-
-   }
-
-   formatPopup(name: string):string{
-
-    var html = "<h2>"+name+"</h2>";
-
-    this.instrumentService.getByOrigin(name).subscribe(instrument => this.instruments = instrument);
-    if(this.instruments != null){
-    this.instruments.forEach(instrument => {
-      
-      html += "<h2>"+instrument.name+"</h2>"
-      
-      
+      instruments.forEach((instrument) => {
+        html += '<div id = "grid-element">' + '<a href = "#">' + instrument.name + '</a><img src = "' + instrument.img + '">' + "</div>";
+      });
     });
 
-   
-
-    }
-
-    return(html)
-
-   }
+    return name + html;
+  }
 }
--- app/src/app/map/map.component.ts
+++ app/src/app/map/map.component.ts
@@ -1,100 +1,88 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, AfterViewInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Component, AfterViewInit} from "@angular/core";
 
-import * as L from 'leaflet';
-import { Observable } from 'rxjs';
-import { Instrument } from '../core/models/instrument';
-import { InstrumentService } from '../core/services/http/instrument.service';
-import { PopupService } from '../core/services/http/popup.service';
-import { ShapeService } from '../core/services/map/shape.service';
+import * as L from "leaflet";
+import {Observable} from "rxjs";
+import {Instrument} from "../core/models/instrument";
+import {InstrumentService} from "../core/services/http/instrument.service";
+import {PopupService} from "../core/services/http/popup.service";
+import {ShapeService} from "../core/services/map/shape.service";
 
-
-@Component({
-  selector: 'app-map',
-  templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
-})
-
+@Component({selector: "app-map", templateUrl: "./map.component.html", styleUrls: ["./map.component.scss"]})
 export class MapComponent implements AfterViewInit {
+  private STYLES = {
+    weight: 2,
+    opacity: 0.9,
+    color: "#C2185B",
+    fillOpacity: 0.8,
+    fillColor: "#1a1a1a"
+  };
 
-  
-private STYLES = ({
-  weight: 2,
-  opacity: 0.9,
-  color: '#C2185B',
-  fillOpacity: 0.8,
-  fillColor: '#1a1a1a'
-});
+  private STYLES_CLICKED = {
+    weight: 2,
+    opacity: 1.0,
+    color: "#9f144b",
+    fillOpacity: 0.8,
+    fillColor: "#C2185B"
+  };
 
-private STYLES_CLICKED = ({
-  weight: 2,
-  opacity: 1.0,
-  color: '#9f144b',
-  fillOpacity: 0.8,
-  fillColor: '#C2185B'
-});
+  private map: any;
 
-  private map:any;
+  private shapes: any;
 
-  private shapes:any;
+  private instruments$?: Observable<Instrument[]>;
+  private instruments?: Instrument[];
 
-private instruments?: Observable<Instrument[]>;
-
-
-  constructor(private shapeService:ShapeService, private popupService: PopupService, private instrumentService: InstrumentService, private http: HttpClient) { }
+  constructor(private shapeService : ShapeService, private popupService : PopupService, private instrumentService : InstrumentService, private http : HttpClient) {}
 
   ngAfterViewInit(): void {
     this.initMap();
-    
-    this.shapeService.get().subscribe(shapes => {
+
+    this.shapeService.get().subscribe((shapes) => {
       this.shapes = shapes;
-      this.initLayer()
+      this.initLayer();
     });
   }
 
-
-
   private initMap(): void {
-    this.map = L.map('map', {
-      center: [ 46.22, 2.2 ],
+    this.map = L.map("map", {
+      center: [
+        46.22, 2.2
+      ],
       zoom: 3
     });
 
-    const tiles = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+    const tiles = L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", {
       maxZoom: 18,
-      minZoom: 1,
+      minZoom: 1
     });
 
     tiles.addTo(this.map);
   }
 
-//SHAPES
-private initLayer() {
-  const Layer = L.geoJSON(this.shapes, {
-    style: (feature) => this.STYLES,
-    
-    onEachFeature: (feature, layer) => (
-      
-      layer.on({
-        mouseover: (e) => (this.highlightFeature(e)),
-        mouseout: (e) => (this.resetFeature(e)),
-        click: (event) => (layer.bindPopup(this.popupService.makePopup(feature)))
-      })
-    )
-  }).addTo(this.map);
+  //SHAPES
+  private initLayer() {
+    const Layer = L.geoJSON(this.shapes, {
+      style: (feature) => this.STYLES,
 
+      onEachFeature: (feature, layer) => (layer.bindPopup(this.popupService.makePopup(feature.properties.ADMIN)), layer.on({
+        mouseover: (e) => this.highlightFeature(e),
+        mouseout: (e) => this.resetFeature(e)
+        // mousedown: (e) => layer.bindPopup(this.popupService.makePopup(feature.properties.ADMIN))
+      }))
+    });
+    this.map.addLayer(Layer);
+  }
 
-}
+  private highlightFeature(e : any) {
+    const layer = e.target;
 
-private highlightFeature(e:any) {
-  const layer = e.target;
+    layer.setStyle(this.STYLES_CLICKED);
+  }
 
-  layer.setStyle(this.STYLES_CLICKED)
-}
+  private resetFeature(e : any) {
+    const layer = e.target;
 
-private resetFeature(e:any) {
-  const layer = e.target;
-
-  layer.setStyle(this.STYLES);
-}
+    layer.setStyle(this.STYLES);
+  }
 }
